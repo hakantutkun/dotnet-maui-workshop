13e0ef9f446358aef7820ab545d5d2c7b244ae6a
hakan <hakan@DESKTOP-7IJG156.local>
2022-08-27 01:07:09 +0300

    ToDo Model eklendi.

---
+++ ToDoApp.MAUI/ToDoApp.MAUI/Models/ToDo.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace ToDoApp.MAUI.Models
+{
+    public class ToDo : INotifyPropertyChanged
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set 
+            {
+                if (_id == value)
+                    return;
+
+                _id = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Id)));
+            }
+        }
+
+        private string _todoName;
+
+        public string TodoName
+        {
+            get { return _todoName; }
+            set 
+            {
+                if (_todoName == value)
+                    return;
+
+                _todoName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TodoName)));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
