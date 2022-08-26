cd2a1195bb0aad55254f95e16835600a4b377f51
hakan <hakan@DESKTOP-7IJG156.local>
2022-08-27 00:16:38 +0300

    Api projesi tamamlandı.

---
+++ TodoApp.API/TodoApp.API/Models/ToDo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.API.Models
+{
+    public class ToDo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? ToDoName { get; set; }
+
+    }
+}
